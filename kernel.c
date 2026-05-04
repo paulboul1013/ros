4011846799504f75fae0a2b2419e710720c666c6
paulboul1013 <paulboul1013@gmail.com>
2026-05-04 16:53:45 +0800

    Add basic type and macros and memory functions

--- kernel.c
+++ kernel.c
@@ -1,9 +1,7 @@
 #include "kernel.h"
 #include "common.h"
 
-typedef unsigned char uint8_t;
-typedef unsigned int uint32_t;
-typedef uint32_t size_t;
+
 
 extern char __bss[], __bss_end[], __stack_top[];
 
