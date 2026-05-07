e54bee2ab8474d9933e5a54be7e41d1bea61ede1
paulboul1013 <paulboul1013@gmail.com>
2026-05-07 15:14:42 +0800

    change to origin sh name

--- run.sh
+++ run.sh
@@ -4,13 +4,22 @@ set -xue
 # qemu file path
 QEMU=qemu-system-riscv32
 
+OBJCOPY=/usr/bin/llvm-objcopy
+
+
 # path to clang and compiler flags
 CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unkown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
+# Build the shell (application)
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
+
 # build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 # start qemu
 $QEMU -machine virt -bios default -nographic -serial mon:stdio -no-reboot \
