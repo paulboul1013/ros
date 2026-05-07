d0a1f926c3dfe7ea6c50243258e5e7fca3dc2157
paulboul1013 <paulboul1013@gmail.com>
2026-05-07 15:37:37 +0800

    stop show process print

--- kernel.c
+++ kernel.c
@@ -197,15 +197,15 @@ void kernel_main(void){
 
     WRITE_CSR(stvec,(uint32_t)kernel_entry);
 
-    idle_proc=create_process((uint32_t)NULL);
-    idle_proc->pid=0; //idle
-    current_proc=idle_proc;
+    // idle_proc=create_process((uint32_t)NULL);
+    // idle_proc->pid=0; //idle
+    // current_proc=idle_proc;
 
-    proc_a=create_process((uint32_t)proc_a_entry);
-    proc_b=create_process((uint32_t)proc_b_entry);
+    // proc_a=create_process((uint32_t)proc_a_entry);
+    // proc_b=create_process((uint32_t)proc_b_entry);
     
-    yield();
-    PANIC("switched to idle process");
+    // yield();
+    // PANIC("switched to idle process");
 }
 
 __attribute__((section(".text.boot")))
