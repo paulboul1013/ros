cc94841e89f169c52b5935bf82f56f3182ab1a9e
paulboul1013 <paulboul1013@gmail.com>
2026-05-07 16:49:32 +0800

    implement syscall interface

--- shell.c
+++ shell.c
@@ -1,8 +1,19 @@
 #include "user.h"
 
+int syscall(int sysno, int arg0,int arg1,int arg2){
+    register int a0 __asm__("a0")=arg0;
+    register int a1 __asm__("a1")=arg1;
+    register int a2 __asm__("a2")=arg2;
+    register int a3 __asm__("a3")=sysno;
 
+    __asm__ __volatile__("ecall"
+                        : "=r"(a0)
+                        : "r"(a0), "r"(a1), "r"(a2), "r"(a3)
+                        : "memory");
+
+    return a0;
+}
 
 void main(void) {
-    *((volatile int *)0x80200000) =0x1234;
-    for(;;);
+    printf("Hello World from shell!\n");
 }
