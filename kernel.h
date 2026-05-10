d7fe256ca32c9d9e21416288d0275cefa5c8e458
paulboul1013 <paulboul1013@gmail.com>
2026-05-10 19:25:22 +0800

    define file and tar_header struct

--- kernel.h
+++ kernel.h
@@ -22,6 +22,8 @@
 #define SCAUSE_ECALL 8 //syscall
 #define PROC_EXITED 2
 
+#define SSTATUS_SUM (1<<18) 
+
 #define SECTOR_SIZE       512
 #define VIRTQ_ENTRY_NUM   16
 #define VIRTIO_DEVICE_BLK 2
@@ -46,6 +48,37 @@
 #define VIRTQ_AVAIL_F_NO_INTERRUPT 1
 #define VIRTIO_BLK_T_IN  0
 #define VIRTIO_BLK_T_OUT 1
+
+#define FILES_MAX 2
+#define DISK_MAX_SIZE align_up(sizeof(struct file) * FILES_MAX, SECTOR_SIZE)
+
+struct file {
+    bool in_use; // indicates if this file entry is in use
+    char name[100]; //file name
+    char data[1024]; //file content
+    size_t size; //file size
+};
+
+struct tar_header {
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char checksum[8];
+    char type;
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char padding[12];
+    char data[]; //array pointing to the data area following the header (flexiable array member)
+} __attribute__((packed));
 
 //virtqueue descriptor table entry
 struct virtq_desc {
