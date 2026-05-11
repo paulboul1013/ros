46802c01d09c576dc0e5329890186de95abc2daf
paulboul1013 <paulboul1013@gmail.com>
2026-05-11 16:35:05 +0800

    Add backspace keyboard detect and implement backspace delete characters

--- shell.c
+++ shell.c
@@ -1,5 +1,19 @@
 #include "user.h"
 
+#define CMDLINE_MAX 128
+#define ASCII_BS '\b'
+#define ASCII_DEL 0x7f
+
+static int is_backspace(char ch) {
+    return ch == ASCII_BS || ch == ASCII_DEL;
+}
+
+static void erase_last_char_on_screen(void) {
+    putchar('\b');
+    putchar(' ');
+    putchar('\b');
+}
+
 int syscall(int sysno, int arg0,int arg1,int arg2){
     register int a0 __asm__("a0")=arg0;
     register int a1 __asm__("a1")=arg1;
@@ -18,21 +32,35 @@ void main(void) {
     while(1){
 prompt:
         printf("> ");
-        char cmdline[128];
-        for(int i=0;;i++) {
+        char cmdline[CMDLINE_MAX];
+        int len=0;
+        
+        for(;;) {
             char ch=getchar();
-            putchar(ch);
-            if (i==sizeof(cmdline)-1){ 
+            
+
+            if (ch=='\r') { //newline character is \r in the debug qemu terminal
+                printf("\n");
+                cmdline[len]=0;
+                break;
+            }
+
+            if (is_backspace(ch)) {
+                if (len>0) {
+                    len--;
+                    erase_last_char_on_screen();
+                }
+                continue;
+            }
+
+            if (len==CMDLINE_MAX-1){ 
                 printf("command line too long\n");
                 goto prompt;
             }
-            else if (ch=='\r') { //newline character is \r in the debug qemu terminal
-                printf("\n");
-                cmdline[i]=0;
-                break;
-            }else{
-                cmdline[i]=ch;
-            }
+
+            cmdline[len]=ch;
+            len++;
+            putchar(ch);
         }            
 
         if (strcmp(cmdline,"hello")==0){
